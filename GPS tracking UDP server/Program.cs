481948f90f2013ab8e190b9709c644ba68383b5e
hainguyen9x4 <hainguyen9x4@gmail.com>
2024-08-21 09:17:53 +0700

    Add files via upload

---
+++ GPS tracking UDP server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UdpSrvrSample
+{
+    public static void Main()
+    {
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("103.82.22.245"), 9050);
+        UdpClient newsock = new UdpClient(ipep);
+
+        Console.WriteLine("Waiting for a client...");
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+        data = newsock.Receive(ref sender);
+
+        Console.WriteLine("Message received from {0}:", sender.ToString());
+        var sDatas = Encoding.ASCII.GetString(data, 0, data.Length);
+        Console.WriteLine(sDatas);
+
+        string welcome = "Welcome to my test server";
+        data = Encoding.ASCII.GetBytes(welcome);
+        newsock.Send(data, data.Length, sender);
+
+        while (true)
+        {
+            data = newsock.Receive(ref sender);
+
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+            newsock.Send(data, data.Length, sender);
+        }
+    }
+}
